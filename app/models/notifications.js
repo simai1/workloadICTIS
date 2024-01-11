6eb3abed296374a2fbdc3cc053290dc03e3aa254
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-01-11 18:36:25 +0300

    add notifications model

---
+++ app/models/notifications.js
@@ -0,0 +1,20 @@
+import { DataTypes, Model } from 'sequelize';
+
+export default class Notification extends Model {
+	static initialize(sequelize) {
+		Notification.init(
+			{
+				message: { type: DataTypes.STRING, allowNull: false },
+				receiverRoles: { type: DataTypes.ARRAY(DataTypes.SMALLINT) },
+				isChecked: { type: DataTypes.BOOLEAN, defaultValue: false },
+			},
+			{
+				sequelize,
+				schema: 'public',
+				modelName: 'Notification',
+				tableName: 'notifications',
+				paranoid: true,
+			}
+		);
+	}
+}
