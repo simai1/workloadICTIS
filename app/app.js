99ab354973aef85c0560dbf206103d55c6ccd3f7
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-01-11 18:36:52 +0300

    add db init

--- app/app.js
+++ app/app.js
@@ -1,22 +1,20 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import corsMiddleware from './middlewares/cors.js';
-// import dbUtils from './utils/db.js';
-// import authRoute from './routes/auth.js';
+import dbUtils from './utils/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// (async function initDb() {
-//     try {
-//         await dbUtils.initializeDbModels();
-//     } catch (e) {
-//         console.log(e);
-//         console.log('COULD NOT CONNECT TO THE DB, retrying in 5 seconds');
-//         setTimeout(initDb, 5000);
-//     }
-//
-// })();
+(async function initDb() {
+	try {
+		await dbUtils.initializeDbModels();
+	} catch (e) {
+		console.log(e);
+		console.log('COULD NOT CONNECT TO THE DB, retrying in 5 seconds');
+		setTimeout(initDb, 5000);
+	}
+})();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
@@ -24,5 +22,5 @@ app.use(cookieParser());
 app.use(corsMiddleware);
 
 // app.use('/auth', authRoute);
-
+console.log(`Node env: ${process.env.NODE_ENV}`);
 app.listen(PORT, () => console.log(`Listen on :${PORT}`));
---
+++ app/models/index.js
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize';
+import Notification from './notifications.js';
+
+const { DB_USER, DB_PWD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+
+export const models = {
+	Notification,
+};
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PWD, {
+	host: DB_HOST,
+	port: DB_PORT,
+	dialect: 'postgres',
+	dialectOptions: {
+		//multipleStatements: true,
+		typeCast: true,
+	},
+	define: {
+		// charset: 'utf8mb4',
+		// collate: 'utf8mb4_unicode_ci',
+		timestamps: true,
+		underscored: true,
+	},
+	logging: false,
+});
