96059474dad02941d2cbe93136fbb1ff9e12617b
hoTiwe <zerftiv125@gmail.com>
2024-01-16 15:12:57 +0300

    add module for parser from excel file and show him

---
+++ app/routes/parser.js
@@ -0,0 +1,28 @@
+import {Router} from "express";
+import multer from "multer";
+import {asyncRoute} from "../utils/errors.js";
+import parserController from '../controllers/parser.js';
+
+const router = Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]);
+    }
+});
+
+const upload = multer({ // multer settings
+    storage: storage
+});
+// fixme: привести к человеческому виду
+router.post('/xlsx', upload.single('file'), function (req, res) {
+    const fileLocation = req.file.path;
+    console.log(fileLocation); // logs uploads/file-1541675389394.xls
+    asyncRoute(parserController.parseFromXlsx(fileLocation, res));
+});
+
+export default router;
