f6a9723c43c1bbe2e71750d086ded2b183bce076
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-01-17 13:00:05 +0300

    fix typo

--- app/utils/educators-hours.js
+++ app/utils/educators-hours.js
@@ -1,10 +1,10 @@
-import recomendHours from '../config/recomend-hours.js';
+import recommendHours from '../config/recommend-hours.js';
 import { map as mapPosition } from '../config/position.js';
 
 export function setHours(educator) {
     const rate = educator.get('rate');
     const position = mapPosition[educator.get('position')];
     educator.set('maxHours', rate * 900);
-    educator.set('recommendedMaxHours', recomendHours[position].max * rate);
-    educator.set('minHours', recomendHours[position].min * rate);
+    educator.set('recommendedMaxHours', recommendHours[position].max * rate);
+    educator.set('minHours', recommendHours[position].min * rate);
 }
