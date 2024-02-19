68f433de34016bd2816057592afd362e3412c6d1
hoTiwe <zerftiv125@gmail.com>
2024-02-19 12:25:03 +0300

    example to parser for save data

--- app/controllers/parser.js
+++ app/controllers/parser.js
@@ -1,21 +1,87 @@
 import XLSX from "xlsx";
+import Educator from "../models/educator.js";
+import {DataTypes} from "sequelize";
+import EnumDepartments from "../config/departments.js";
+import {AppErrorNotExist} from "../utils/errors.js";
+import Workload from "../models/workload.js";
+import EducatorForWorkload from "../models/educator-for-workload.js";
 
 export default {
     async parseFromXlsx(fileLocation, res){
         let workbook = XLSX.readFile(fileLocation);
-        const dataWorkload = [];
+        const workload = [];
 
         Object.keys(workbook.Sheets).forEach((name) => {
             const sheetData = XLSX.utils.sheet_to_json(workbook.Sheets[name], {header: 1});
-            dataWorkload.push({ name, data: sheetData });
+            workload.push({ name, data: sheetData });
         });
 
-        dataWorkload.forEach((element) =>{
-            console.log(element.data[0])
-            console.log(element.data[1])
+        const dataWorkload = workload[0].data;
+        for (const element of dataWorkload) {
+            const id = element[0];
+            if(Number(id)) {
+                try {
+                    const discipline = element[2];
+                    const workload = element[3];
+                    const groups = element[4];
+                    const block = element[5];
+                    const semester = element[6];
+                    const period = element[7];
+                    const curriculum = element[8];
+                    const curriculumUnit = element[9];
+                    const formOfEducation = element[11];
+                    const levelOfTraining = element[12];
+                    const specialty = element[13];
+                    const core = element[14];
+                    const numberOfStudents = Number(element[16].replace(',00',''));
+                    const hours = parseFloat(element[17].replace(',','.'));
+                    const audienceHours = parseFloat(element[18].replace(',','.'));
+                    const ratingControlHours = hours - audienceHours;
+                    const nameEducator = element[21];
+                    //
+                    const department = 2;
 
-        })
+                    console.log('nameEducator')
+                    console.log(nameEducator)
+                    const existEducator = await Educator.findOne({
+                        where: {
+                            name: nameEducator,
+                        }
+                    });
+                    console.log(existEducator?.dataValues)
+                    if(!existEducator) throw new AppErrorNotExist('educator');
+                    const newWorkload = await Workload.create({
+                        discipline,
+                        department,
+                        workload,
+                        groups,
+                        block,
+                        semester,
+                        period,
+                        curriculum,
+                        curriculumUnit,
+                        formOfEducation,
+                        levelOfTraining,
+                        specialty,
+                        core,
+                        numberOfStudents,
+                        hours,
+                        audienceHours,
+                        ratingControlHours,
+                    })
 
-        return res.json(dataWorkload[0].data);
+                    await EducatorForWorkload.create({
+                        EducatorId: existEducator.id,
+                        WorkloadId: newWorkload.id,
+                    })
+
+                } catch (e) {
+                    console.log(e)
+                }
+
+            }
+        }
+
+        return res.json(dataWorkload);
     }
 }
