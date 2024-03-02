eef1b8ffd35f92c219523dcaa979afa7e9df4822
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-03-02 17:46:34 +0300

    add map

--- app/dtos/educator-dto.js
+++ app/dtos/educator-dto.js
@@ -1,22 +1,27 @@
+import { map as positionsMap } from '../config/position.js';
+import { map as typeMap } from '../config/type-of-employment.js';
+import { map as departmentsMap } from '../config/departments.js';
+
 export default class EducatorDto {
-  id;
-  name;
-  position;
-  typeOfEmployment;
-  department;
-  rate;
-  maxHours;
-  recommendedMaxHours;
-  minHours;
-  constructor(model) {
-    this.id = model.id;
-    this.name = model.name;
-    this.position = model.position;
-    this.typeOfEmployment = model.typeOfEmployment;
-    this.rate = model.rate;
-    this.maxHours = model.maxHours;
-    this.recommendedMaxHours = model.recommendedMaxHours;
-    this.minHours = model.minHours;
-    this.department = model.department;
-  }
+    id;
+    name;
+    position;
+    typeOfEmployment;
+    department;
+    rate;
+    maxHours;
+    recommendedMaxHours;
+    minHours;
+
+    constructor(model) {
+        this.id = model.id;
+        this.name = model.name;
+        this.position = positionsMap[model.position];
+        this.typeOfEmployment = typeMap[model.typeOfEmployment];
+        this.rate = model.rate;
+        this.maxHours = model.maxHours;
+        this.recommendedMaxHours = model.recommendedMaxHours;
+        this.minHours = model.minHours;
+        this.department = departmentsMap[model.department];
+    }
 }
