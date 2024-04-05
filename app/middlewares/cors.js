ca1d6ca74035033c220deebb3a85b05cc58edd97
RpgGold <101806485+RpgGolden@users.noreply.github.com>
2024-04-05 21:49:28 +0300

    вернул как было

--- app/middlewares/cors.js
+++ app/middlewares/cors.js
@@ -2,9 +2,7 @@ import cors from 'cors';
 
 export default cors({
     credentials: true,
-    // origin: true,
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    origin: true,
     exposedHeaders: '*',
     allowedHeaders: '*',
 });
