ec6a7333e1421772bb82a6292f4a8fa4a7bf4a9f
RpgGold <101806485+RpgGolden@users.noreply.github.com>
2024-04-05 21:22:49 +0300

    update cors

--- app/middlewares/cors.js
+++ app/middlewares/cors.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 export default cors({
     credentials: true,
     // origin: true,
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     exposedHeaders: '*',
     allowedHeaders: '*',
