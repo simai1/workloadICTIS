fcbb8d52b2fd0780241e281b6921665701846454
hoTiwe <zerftiv125@gmail.com>
2024-04-25 13:49:16 +0300

    .

--- workload/src/pages/Authorization/Authorization.jsx
+++ workload/src/pages/Authorization/Authorization.jsx
@@ -9,7 +9,7 @@ function Authorization() {
         <h2>Авторизация</h2>
         <a
           className={styles.button}
-          href="http://workload.sfedu.ru/auth/loginSfedu"
+          href="https://workload.sfedu.ru/auth/loginSfedu"
         >
           Войти через сервис Microsoft
         </a>
