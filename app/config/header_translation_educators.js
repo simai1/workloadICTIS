ee82440d2efc6ca35cc9df8c62c414003bbee56e
hoTiwe <zerftiv125@gmail.com>
2024-05-31 23:07:48 +0300

    .

---
+++ app/config/header_translation_educators.js
@@ -0,0 +1,10 @@
+const HeaderTranslation = {
+    'ФИО':'name',
+    'Сруктурное подразделение':'',
+    'Должность':'position',
+    'Ставка':'rate',
+    'Почта':'email',
+};
+
+
+export default HeaderTranslation;
