cccb45435802474f5ab70acea793defa41c7e204
Никита <100412266+Niks6455@users.noreply.github.com>
2024-06-14 16:25:51 +0300

    фыв

--- workload/src/pages/HomePage/Homepage.jsx
+++ workload/src/pages/HomePage/Homepage.jsx
@@ -269,9 +269,7 @@ function HomePage() {
 
                 {appData.metodRole[appData.myProfile?.role]?.some(
                   (el) => el === 27
-                ) &&
-                  ((basicTabData.nameKaf != "Все" && cafedral) ||
-                    (basicTabData.nameKaf === "Все" && !cafedral)) && (
+                ) && (basicTabData.nameKaf != "Все кафедры") && (
                     <div
                       style={{ marginRight: "20px" }}
                       className={styles.btnMenuBox}
