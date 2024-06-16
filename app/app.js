489b7ee63774f9ffaf57f44a4ab80d031f86f8b8
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-16 07:10:34 +0300

    change session time

--- app/app.js
+++ app/app.js
@@ -63,7 +63,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     cookieSession({
-        maxAge: 24 * 60 * 60 * 1000,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
         keys: [process.env.COOKIE_KEY],
     })
 );
