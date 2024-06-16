489cd3d6494843f1737881668a4aa8f62ebb2a26
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-06-16 07:16:08 +0300

    delete type of employment

--- app/dtos/educator-profile-dto.js
+++ app/dtos/educator-profile-dto.js
@@ -1,12 +1,10 @@
 import { map as positionsMap } from '../config/position.js';
-import { map as typeMap } from '../config/type-of-employment.js';
 import { map as departmentsMap } from '../config/departments.js';
 
 export default class EducatorProfileDto {
   id;
   name;
   position;
-  typeOfEmployment;
   department;
   rate;
   totalHours;
@@ -16,7 +14,6 @@ export default class EducatorProfileDto {
     this.id = model.id;
     this.name = model.name;
     this.position = positionsMap[model.position];
-    this.typeOfEmployment = typeMap[model.typeOfEmployment];
     this.department = departmentsMap[model.department];
     this.rate = model.rate;
     this.totalHours = model.SummaryWorkload.totalHours;
