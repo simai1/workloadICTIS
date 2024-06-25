d44ee2d9c8e320742d49702bbe46239eba72d787
hoTiwe <zerftiv125@gmail.com>
2024-06-25 15:26:11 +0300

    пред комит

--- workload/src/api/services/ApiRequest.js
+++ workload/src/api/services/ApiRequest.js
@@ -1,8 +1,8 @@
 //? Здесь все запросы к апи, присвоение этих данных состояниями в AssingApiData
 
 import axios from "axios";
-const server = "https://workload.sfedu.ru";
-// const server = "http://localhost:3002"
+//const server = "https://workload.sfedu.ru";
+const server = "http://localhost:3002"
 // const server = process.env.REACT_APP_API_URL;
 const http = axios.create({
   withCredentials: true,
