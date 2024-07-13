b83837694e7d867047044f5a829a4bb5c04a3e15
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-07-13 11:50:13 +0300

    add guide notification setup

--- app/config/email-templates.js
+++ app/config/email-templates.js
@@ -30,5 +30,11 @@ export default {
           <img src="https://sun1-56.userapi.com/impg/-CvTmoJz2DpEL3yYq87pDeuPhvAyTe3RsxhHpw/y7i_YQeO9ns.jpg?size=586x512&quality=96&sign=278bff9c4e07d6fdf35aa6bac2225c7b&type=album" alt="1">
           <img src="https://sun9-70.userapi.com/impg/XsCsX2BGsmJEvlGUYxtlEQmBCnP68vuadQutbg/Wq97lDuq98Q.jpg?size=334x239&quality=96&sign=2762806ca4fdfe7ae74170e1940e722a&type=album" alt="2">
           <p>(Все действия на скриншотах выполнены на тестовом сервере)</p>`
+  },
+  guideAppears: {
+    subject: 'Уведомление workload.sfedu.ru',
+    template: () =>
+      `<p>Руководство по пользованию системой уже доступно!<br>
+          Вы можете ознакомиться с ним нажав на кнопку с восклицательным знаком в правом верхнем углу страницы.</p>`
   }
 };
