a3e749f286c24ad64f31cfb29b848107bef8ac12
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-07-17 20:07:49 +0300

    fix

--- app/config/email-templates.js
+++ app/config/email-templates.js
@@ -27,8 +27,8 @@ export default {
           в <a href="https://workload.sfedu.ru/client/">системе распределения нагрузки</a><br>
           Для этого нажмите правой кнопкой мыши по нагрузке -> выберите пункт "Предложить" -> выберите преподавателя -> подтвердите действие. <br>
           После этого слева от нагрузки появится желтый кружок, кликнув по которому, можно увидеть предложение.</p>
-          <img src="../static/y7i_YQeO9ns.jpg" alt="1">
-          <img src="../static/Wq97lDuq98Q.jpg" alt="2">
+          <img src="https://sun1-56.userapi.com/impg/-CvTmoJz2DpEL3yYq87pDeuPhvAyTe3RsxhHpw/y7i_YQeO9ns.jpg?size=586x512&quality=96&sign=278bff9c4e07d6fdf35aa6bac2225c7b&type=album" alt="1">
+          <img src="https://sun9-70.userapi.com/impg/XsCsX2BGsmJEvlGUYxtlEQmBCnP68vuadQutbg/Wq97lDuq98Q.jpg?size=334x239&quality=96&sign=2762806ca4fdfe7ae74170e1940e722a&type=album" alt="2">
           <p>(Все действия на скриншотах выполнены на тестовом сервере)</p><br><br>
           <p>Елькин Дмитрий Максимович, Руководитель центра разработки ИКТИБ</p>`
   },
