e484532c306665be8b3dd176371013a32735a3ec
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-07-23 15:06:06 +0300

    add materials

---
+++ app/controllers/materials.js
@@ -0,0 +1,89 @@
+import Workload from '../models/workload.js';
+import { Op } from 'sequelize';
+import Educator from '../models/educator.js';
+import MaterialsDto from '../dtos/materials-dto.js';
+import Materials from '../models/materials.js';
+import { AppErrorMissing } from '../utils/errors.js';
+
+export default {
+    async sync(req, res) {
+        const workloads = await Workload.findAll({
+            where: {
+                audienceHours: { [Op.ne]: 0 },
+                educatorId: { [Op.ne]: null },
+            },
+            include: [{ model: Educator }],
+        });
+        const materials = [...new Set(workloads.map(w => JSON.stringify(new MaterialsDto(w))))];
+        const uniqueWorkloads = [];
+        let wmat;
+        workloads.forEach(w => {
+            wmat = JSON.stringify(new MaterialsDto(w));
+            if (materials.includes(wmat)) {
+                uniqueWorkloads.push(w);
+                delete materials[materials.indexOf(wmat)];
+            }
+        });
+        await Materials.bulkCreate(
+            uniqueWorkloads.map(w =>
+                Object({
+                    fields: JSON.stringify(new MaterialsDto(w)),
+                    department: w.department,
+                    discipline: w.discipline,
+                    workload: w.workload,
+                    groups: w.groups,
+                    block: w.block,
+                    semester: w.semester,
+                    period: w.period,
+                    curriculum: w.curriculum,
+                    curriculumUnit: w.curriculumUnit,
+                    formOfEducation: w.formOfEducation,
+                    levelOfTraining: w.levelOfTraining,
+                    specialty: w.specialty,
+                    core: w.core,
+                    numberOfStudents: w.numberOfStudents,
+                    hours: w.hours,
+                    audienceHours: w.audienceHours,
+                    ratingControlHours: w.ratingControlHours,
+                    isSplit: w.isSplit,
+                    isMerged: w.isMerged,
+                    educatorId: w.educatorId,
+                    notes: w.notes,
+                })
+            ),
+            { ignoreDuplicates: true }
+        );
+        res.json({ status: 'OK' });
+    },
+
+    async deleteMaterial(req, res) {
+        const { materialId } = req.params;
+        if (!materialId) throw new AppErrorMissing('materialId');
+        await Materials.destroy({ where: { id: materialId } });
+        res.json({ status: 'OK' });
+    },
+
+    async getAll(req, res) {
+        let { departments } = req.query;
+        departments = departments.split(',').map(d => parseInt(d));
+        let materials;
+        if (!departments) {
+            materials = await Materials.findAll();
+        } else {
+            materials = await Materials.findAll({
+                where: { department: departments },
+                attributes: { exclude: ['fields'] },
+            });
+        }
+        res.json(materials);
+    },
+
+    async setNote(req, res) {
+        const { notes } = req.body;
+        const { materialId } = req.params;
+        if (!notes) throw new AppErrorMissing('notes');
+        if (!materialId) throw new AppErrorMissing('materialId');
+        await Materials.update({ notes }, { where: { id: materialId } });
+        res.json({ status: 'OK' });
+    },
+};
---
+++ app/dtos/materials-dto.js
@@ -0,0 +1,42 @@
+import EducatorDto from './educator-dto.js';
+import { map as departmentsMap } from '../config/departments.js';
+
+export default class MaterialsDto {
+    department;
+    discipline;
+    workload;
+    groups;
+    block;
+    semester;
+    period;
+    curriculum;
+    curriculumUnit;
+    formOfEducation;
+    levelOfTraining;
+    specialty;
+    core;
+    hours;
+    audienceHours;
+    ratingControlHours;
+    educator;
+
+    constructor(model) {
+        this.department = departmentsMap[model.department];
+        this.discipline = model.discipline;
+        this.workload = model.workload;
+        this.groups = model.groups;
+        this.block = model.block;
+        this.semester = model.semester;
+        this.period = model.period;
+        this.curriculum = model.curriculum;
+        this.curriculumUnit = model.curriculumUnit;
+        this.formOfEducation = model.formOfEducation;
+        this.levelOfTraining = model.levelOfTraining;
+        this.specialty = model.specialty;
+        this.core = model.core;
+        this.hours = model.hours;
+        this.audienceHours = model.audienceHours;
+        this.ratingControlHours = model.ratingControlHours;
+        this.educator = model.Educator ? new EducatorDto(model.Educator) : null;
+    }
+}
