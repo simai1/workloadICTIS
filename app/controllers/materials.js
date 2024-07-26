304b65e22b7b195ae073ea14863b1c7bea30b68d
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-07-26 13:51:17 +0300

    add getMaterials by roles

--- app/controllers/materials.js
+++ app/controllers/materials.js
@@ -4,7 +4,7 @@ import Educator from '../models/educator.js';
 import MaterialsDto from '../dtos/materials-dto.js';
 import Materials from '../models/materials.js';
 import { map as mapDepartments } from '../config/departments.js';
-import { AppErrorMissing, AppErrorNotExist } from '../utils/errors.js';
+import { AppErrorInvalid, AppErrorMissing, AppErrorNotExist } from '../utils/errors.js';
 import MaterialsModelDto from '../dtos/materialModel-dto.js';
 import User from '../models/user.js';
 
@@ -84,16 +84,63 @@ export default {
 
     async getAll(req, res) {
         let { departments } = req.query;
+        const user = await User.findByPk(req.user, { include: [{ model: Educator }] });
         let materials;
-        if (!departments) {
+        if ([1, 4, 7, 9, 10].includes(user.role)) {
+            // METHODIST & DIRECTORATE & DEPUTY_DIRECTORATE & GOD & GIGA_ADMIN
+            if (!departments) {
+                materials = await Materials.findAll({
+                    order: orderRule,
+                    attributes: { exclude: ['fields'] },
+                    include: [{ model: Educator }],
+                });
+            } else {
+                departments = departments.split(',').map(d => parseInt(d));
+                materials = await Materials.findAll({
+                    where: { department: departments },
+                    attributes: { exclude: ['fields'] },
+                    include: [{ model: Educator }],
+                    order: orderRule,
+                });
+            }
+        } else if ([3, 8].includes(user.role)) {
+            // DEPARTMENT_HEAD & DEPUTY_DEPARTMENT_HEAD
             materials = await Materials.findAll({
-                order: orderRule,
+                where: { department: user.Educator.department },
+                attributes: { exclude: ['fields'] },
                 include: [{ model: Educator }],
+                order: orderRule,
             });
-        } else {
-            departments = departments.split(',').map(d => parseInt(d));
+        } else if (user.role === 6) {
+            // UNIT_ADMIN
             materials = await Materials.findAll({
-                where: { department: departments },
+                where: { department: user.allowedDepartments },
+                attributes: { exclude: ['fields'] },
+                include: [{ model: Educator }],
+                order: orderRule,
+            });
+            if (!departments) {
+                materials = await Materials.findAll({
+                    where: { department: user.allowedDepartments },
+                    attributes: { exclude: ['fields'] },
+                    order: orderRule,
+                    include: [{ model: Educator }],
+                });
+            } else {
+                departments = departments.split(',').map(d => parseInt(d));
+                if (!departments.every(d => user.allowedDepartments.includes(d)))
+                    throw new AppErrorInvalid('departments');
+                materials = await Materials.findAll({
+                    where: { department: departments },
+                    attributes: { exclude: ['fields'] },
+                    include: [{ model: Educator }],
+                    order: orderRule,
+                });
+            }
+        } else if ([2, 5].includes(user.role)) {
+            // LECTURER & EDUCATOR
+            materials = await Materials.findAll({
+                where: { educatorId: user.Educator.id },
                 attributes: { exclude: ['fields'] },
                 include: [{ model: Educator }],
                 order: orderRule,
