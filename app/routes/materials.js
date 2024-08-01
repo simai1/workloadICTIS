e081202a4e7a8b1ad292444b7e70ace3315dddc3
Sima <92365160+DaiTangoMne@users.noreply.github.com>
2024-08-01 22:45:24 +0300

    change request

--- app/routes/materials.js
+++ app/routes/materials.js
@@ -6,7 +6,7 @@ import verify from '../middlewares/verify-token.js';
 const router = Router();
 router.use(verify.general);
 
-router.route('/').get(asyncRoute(materialsController.getAll)).patch(asyncRoute(materialsController.update));
+router.route('/').post(asyncRoute(materialsController.getAll)).patch(asyncRoute(materialsController.update));
 router.route('/sync').get(asyncRoute(materialsController.sync));
 router.route('/:materialId').delete(asyncRoute(materialsController.deleteMaterial));
 router.route('/getUsableDepartments').get(asyncRoute(materialsController.getUsableDepartments));
